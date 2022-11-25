3d528a556d6cf8a5d5333e672ad18d6460117470
gang.chen1 <gang.chen1@yintech.cn>
2022-11-25 18:08:15 +0800

    feat: rollup import json

--- build/banner.js
+++ build/banner.js
@@ -1,9 +1,10 @@
 
-// import pkg from '../package.json'
-const pkg = {}
-const version = pkg.version
-const homepage = pkg.homepage
-
+import { readFileSync } from 'fs';
+const pkg = JSON.parse(readFileSync(new URL('../package.json', import.meta.url)));
+const {
+  homepage,
+  version
+} = pkg
 export default `
 /*!
  * v3-virtual-list v${version}
