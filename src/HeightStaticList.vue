4d8068612e19bed5a3f51bb1bdf9c0d0396767b6
coveychen95 <coveychen95@outlook.com>
2023-01-03 23:59:57 +0800

    feat: init data

--- src/HeightStaticList.vue
+++ src/HeightStaticList.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
-
+    <Item></Item>
+    <Placeholder></Placeholder>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue';
+import Item from './components/Item.vue'
+import Placeholder from './components/Placeholder.vue'
+import { fetchData } from './mock'
+
+const PLACEHOLDER_COUNT = 5
+const BUFFER_SIZE = 3
+const FIXED_HEIGHT = 100
+let visible_count = BUFFER_SIZE * 2
+
+const anchorItem = reactive({ index: 0, offset: 0 })
+const listData = reactive([])
+const visibleData = reactive([])
+const topPlaceholders = reactive([])
+const bottomPlaceholders = ([])
+const firstAttachedItem = ref(0)
+const lastAttachedItem = ref(0)
+const scrollRunwayEnd = ref(0)
+const lastScrollTop = ref(0)
+
 
 </script>
 
